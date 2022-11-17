276a69e8986aa48234428f855a824196bf6ba051
jasur <jasurabdibayev0@gmail.com>
2022-11-17 14:03:09 +0500

    all

--- model.py
+++ model.py
@@ -1,2 +1,22 @@
 import cv2
 import csv
+
+VIDEO_SOURCE = 0
+
+cap = cv2.VideoCapture(VIDEO_SOURCE)
+suc, image = cap.read()
+
+cv2.imwrite("frame0.jpg", image)
+img = cv2.imread("frame0.jpg")
+
+r = cv2.selectROIs('ROI Selector', img, showCrosshair=False, fromCenter=False)
+
+rlist = r.tolist()
+print(rlist)
+
+with open('data/rois.csv', 'w', newline='') as outf:
+  csvw = csv.writer(outf)
+  csvw.writerows(rlist)
+
+cap.release()
+cv2.destroyAllWindows()
