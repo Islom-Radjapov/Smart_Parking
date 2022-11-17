cf929f7dc1fb70a5597dc5a7977ee4c718afa43c
jasur <jasurabdibayev0@gmail.com>
2022-11-17 14:35:00 +0500

    detector

--- detector.py
+++ detector.py
@@ -36,36 +36,28 @@ cap = cv2.VideoCapture(VIDEO_SOURCE)
 
 # start the live feed
 while True:
-    # set the number of spots to 0
     spots.loc = 0
 
-    # set two frames for the feed
     ret, frame = cap.read()
     ret2, frame2 = cap.read()
 
-    # define the range of pixels and the thresholds for Canny function
     min = cv2.getTrackbarPos('Min pixels', 'parameters')
     max = cv2.getTrackbarPos('Max pixels', 'parameters')
     lowThreshold = cv2.getTrackbarPos('Threshold1', 'parameters')
     highThreshold = cv2.getTrackbarPos('Threshold2', 'parameters')
 
-    # apply the function for every list of coordinates
     for i in range(len(rois)):
         drawRectangle(frame, rois[i][0], rois[i][1], rois[i][2], rois[i][3])
 
-    # adding the number of available spots on the shown image
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, 'Available spots: ' + str(spots.loc), (10, 30), font, 1, (0, 255, 0), 3)
     cv2.imshow('frame', frame)
 
-    # displaying the image with Canny function applied for reference
     canny = cv2.Canny(frame2, lowThreshold, highThreshold)
     cv2.imshow('canny', canny)
 
-    # listen for 'Q' key to stop the stream
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# when everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
