f752c9b9a26913de0581ae206ae51c60b8f44518
islom <radjapov_islom@mail.ru>
2022-11-19 04:00:58 +0500

    add get state

---
+++ get_state.py
@@ -0,0 +1,17 @@
+import cv2
+from sql_code import get_stiuation
+
+camera = cv2.VideoCapture("test_videos/qora.MOV")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+while True:
+    ret, frame = camera.read()
+
+    cv2.putText(frame, f"{get_stiuation()} Raqamli joy bo'sh!", (50, 450), font, 1, (255, 0, 0), 2)
+
+    cv2.imshow('canny',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+camera.release()
+cv2.destroyAllWindows()
