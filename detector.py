27622fe15c2f268872bb2b71865a2ce4fb526b74
student <jasurabdibayev0@gmail.com>
2022-11-18 10:56:39 +0500

    test video

--- detector.py
+++ detector.py
@@ -1,7 +1,9 @@
 from time import sleep
 import cv2
 import csv
-from database import add_stiuation, get_stiuation, get_all
+from database import add_stiuation, get_stiuation
+
+
 
 class spots:
     loc = 0
@@ -54,16 +56,17 @@ while True:
 
 
     for i in range(len(rois)):
+        print("lokation=>",i , "situation=>", get_stiuation(i).fetchone()[0])
         drawRectangle(frame, rois[i][0], rois[i][1], rois[i][2], rois[i][3], i)
 
-    print( "band joy=>", get_all()[0] )
-    print( "bosh joylar=>", get_all()[1] )
+
+
     # font = cv2.FONT_HERSHEY_SIMPLEX
     # cv2.putText(frame, 'Available spots: ' + str(spots.loc), (10, 30), font, 1, (0, 255, 0), 3)
     # print(str(spots.loc))
     # print(spots.loc)
     cv2.imshow('frame', frame)
-    sleep(0.5)
+    sleep(0.2)
 
     canny = cv2.Canny(frame2, lowThreshold, highThreshold)
     cv2.imshow('canny',canny)
